e0cb63fa07715491745e7c9f04b21978be64cd96
feryel <feryel17799@gmail.com>
2025-12-27 09:34:53 +0100

    Ajout de la fonction acces_netmiko

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, Git!")
+def acces_netmiko():
+    # Affiche la date coté routeur
+    # Affiche les interfaces du routeur dans un fichier interfaces.txt
+    print("Connexion au routeur via Netmiko...")
+
+def dire_bonjour():
+    print("Hello, Git!")
+
+dire_bonjour()
